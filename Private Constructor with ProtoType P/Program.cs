88cd5c93a034a368d23f54e4b94b19db04e51d2c
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-24 06:09:47 +0530

    Add project files.

---
+++ Private Constructor with ProtoType P/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Private_Constructor_with_ProtoType_P
+{
+    class Program
+    {
+        private Program()
+        {
+            Console.WriteLine(" Private Constructor Hii:");
+        }
+
+        // opstional 
+
+      /*  public void show()
+        {
+            Console.WriteLine("Hello :");
+        }*/
+        public static Program Getobj()
+        {
+            Program obj = new Program();
+            return obj;
+        }
+    }
+    class p { 
+        static void Main(string[] args)
+        {
+            Program obj2 = Program.Getobj();
+            Program obj3 = Program.Getobj();
+           /* obj2.show();
+            obj3.show();*/
+        }
+    }
+}
